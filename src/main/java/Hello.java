8f8f9e538d2e70fce044993e65374a826e05108b
Shirin Geldiyeva <shirin7271@gmail.com>
2021-01-26 15:27:06 -0500

    watch movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World , B21");
         System.out.println("B21 future SDET learning Git");
+        System.out.println("Ready to go back");
     }   //watching more movie in bedroom
 }
