af335690077caa15960480ab460f1ac9782b3326
Shirin Geldiyeva <shirin7271@gmail.com>
2021-01-26 15:24:18 -0500

    happy TEAM B21

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World , B21");
         System.out.println("B21 future SDET learning Git");
-    }
+    }   //watching more movie in bedroom
 }
