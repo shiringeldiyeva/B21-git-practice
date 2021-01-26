16c32d1ecc7c39300cfd66a9be1c938529420586
Shirin Geldiyeva <shirin7271@gmail.com>
2021-01-26 15:40:54 -0500

    master branch watching on the living room for 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,8 +3,7 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("B21 future SDET learning Git");
 
-        //movie on the phone
-        //movie on the phone25 min
+        //Oh I love this movia really i recommend you
 
     }
 }
