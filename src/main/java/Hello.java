0ad027de3f57df1af1103d6743f4dd9a8135c657
Shirin Geldiyeva <shirin7271@gmail.com>
2021-01-26 15:39:01 -0500

    watch movie on the phone for 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("B21 future SDET learning Git");
+
+        //movie on the phone
+
     }
 }
