2a62884d8a684e4f9d77cd00d358b9c8d2da09d9
Shirin Geldiyeva <shirin7271@gmail.com>
2021-01-26 15:20:21 -0500

    happy B21

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World , B21");
         System.out.println("B21 future SDET learning Git");
     }
 }
